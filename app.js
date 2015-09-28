f87ab39a074a2b24e5c8dd11151c520f15f4f569
Ray Gomez <rayvincent.gomez@gmail.com>
2015-09-28 17:03:58 +0800

    Added mongoose and express-session.

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ var express = require('express')
 var app = express();
 var port = process.env.PORT || 8080;
 
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var morgan = require('morgan');
 
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({secret: 'anystring',
+				 saveUninitialized: true,
+				 resave: true }));
 
 app.use('/', function (req, res){
 	res.send('Our First Express program.');
+	console.log(req.cookies);
+	console.log('==============');
+	console.log(req.session);
 });
 
 app.listen(port);
