ba0b8c9fa6d23e31c8a7c40e832e3c72105809a5
Ray Gomez <rayvincent.gomez@gmail.com>
2015-09-28 14:15:06 +0800

    use express

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var express = require('express')
+var app = express();
+var port = process.env.PORT || 8080;
 
-console.log('Server running at http://127.0.0.1:1337');
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.use('/', function (req, res){
+	res.send('Our First Express program.');
+});
+
+app.listen(port);
+console.log('Server listening on port:', port);
