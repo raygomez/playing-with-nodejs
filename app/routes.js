d9cc244d59f6c4282168d7c9e9c114107693dcc4
Ray Gomez <rayvincent.gomez@gmail.com>
2015-09-29 22:54:52 +0800

    Added user schema and allow us to add entries to table

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,18 @@
+var User = require('./models/user');
 module.exports = function(app) {
 	app.get('/', function(req, res){
 		res.send('Hello World');
 	});
 
 	app.get('/:username/:password', function(req, res) {
-		console.log(req.params.username + ' ' + req.params.password);
+		var newUser = new User();
+		newUser.local.username = req.params.username;
+		newUser.local.password = req.params.password;
+		console.log(newUser.username + ' ' + newUser.password);
+		newUser.save(function(err) {
+			if(err)
+				throw err;
+		});
 		res.send('Hi');
 	});
 
