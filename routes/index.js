82ef4a48885c3773a954af2686f11d2b9f826acc
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-31 23:50:00 +0800

    implemented request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-json');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var client = request.createClient('http://localhost:8080');
+  client.get('api/', function(e, r, body) {
+    res.render('index', { title: 'Express', data: JSON.stringify(body)});
+
+  });
 });
 
 module.exports = router;
