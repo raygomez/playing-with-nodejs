d043381ba0eb7e962faf934f3eeb2f6055b53ef7
Ray Gomez <rayvincent.gomez@gmail.com>
2015-11-01 23:53:30 +0800

    render the tree from json

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,33 @@ var request = require('request-json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   var client = request.createClient('http://localhost:8080');
   client.get('api/', function(e, r, body) {
-    res.render('index', { title: 'Express', data: body});
+    var data = {};
+    data['core'] = {}
+    data['core']['data'] = [];
+
+    var rncs = body['rncs'];
+
+    for(var i = 0; i < rncs.length; i++) {
+      var node = {};
+      var rnc = rncs[i];
+      node['text'] = rnc['name'];
+
+      var btss = rnc['btss'];
+      var children = [];
+      for(var j = 0; j < btss.length; j++) {
+        var child = {};
+        child['text'] = btss[j]['name'];
+        children.push(child);
+      }
+
+      node['children'] = children;
+      data['core']['data'].push(node);
+    }
+    var str = JSON.stringify(data);
+    res.render('index', { title: 'Express', data: JSON.stringify(data)});
 
   });
 });
